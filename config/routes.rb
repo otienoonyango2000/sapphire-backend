a767f4a3111dbb5d778a9d0f03e5fc50d2c371b9
george <otienoonyango2000@gmail.com>
2023-09-02 14:40:35 +0300

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/create'
+  get 'reviews/update'
+  get 'reviews/delete'
+  get 'movies/index'
+  get 'movies/show'
+  get 'movies/create'
+  get 'movies/update'
+  get 'movies/delete'
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
+  get 'users/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
